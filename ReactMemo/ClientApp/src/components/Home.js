81aec6bed3f412e01abbc696f7a7af8e3ad6656e
haeng <ymca0147@naver.com>
2020-07-13 01:47:28 +0900

    초기 커밋.

---
+++ ReactMemo/ClientApp/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  static displayName = Home.name;
+
+  render () {
+    return (
+      <div>
+        <h1>리액트앱에 오실걸 환영합니다</h1>
+      </div>
+    );
+  }
+}
