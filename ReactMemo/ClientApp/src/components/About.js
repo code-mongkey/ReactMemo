a0f6a5e1548548866d49003c0c316cdc754a2e44
hgh <ymca0147@naver.com>
2020-07-18 17:22:07 +0900

    4장 강의 완료

--- ReactMemo/ClientApp/src/components/About.js
+++ ReactMemo/ClientApp/src/components/About.js
@@ -1,12 +1,61 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component } from "react";
+import { TechList } from "./Shared/TechList/TechList";
+import { SiteList } from "./Shared/SiteList/SiteList";
 
+export class About extends Component {
+  render() {
+    const title = "정보.";
+    const description = "안녕하세요 ReactMemo 사이트입니다.";
+    return (
+      <>
+        <h2>{title}</h2>
+        <h3>{description}</h3>
 
-export class About extends Component{
-    render(){
-        return (
-            <div>
-                <h1>About ME</h1>
+        <p>이 사이트는 React 학습을 위한 데모 사이트입니다</p>
+        <p>학습이 완료된 후에 현재 사이트가 만들어집니다</p>
+
+        <hr className="mb-5 mt-5" />
+
+        <div className="row">
+          <div className="col-md-6">
+            <div className="thumbnail">
+              <img
+                src="http://placeimg.com/640/480/any"
+                alt="profile"
+                className="img-fluid"
+              />
+              <i>Photo by http://placeimg.com</i>
             </div>
-        );
-    }
+          </div>
+          <div className="col-md-6">
+            <div className="thumbnail">
+              <img
+                src="http://unsplash.it/640/480/?random"
+                alt="profile"
+                className="img-fluid"
+              />
+              <i>Photo by http://unsplash.it</i>
+            </div>
+          </div>
+        </div>
+
+        <hr className="mb-5 mt-5" />
+
+        <div className="row">
+          <div className="col-md-6">
+            <h3>
+              <i className="fa fa-wrench"></i> 현재 사이트에서 사용된 기술리스트
+            </h3>
+            <TechList></TechList>
+          </div>
+          <div className="col-md-6">
+            <h3>
+              <i className="fa fa-sitemap"></i> 현재 사이트와 관련된 추천사이트
+            </h3>
+            <SiteList></SiteList>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
--- ReactMemo/ClientApp/src/components/Contact.js
+++ ReactMemo/ClientApp/src/components/Contact.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
 import { ContactDetails } from "./ContactDetails/ContactDetails";
+import { TechList } from "./Shared/TechList/TechList";
+import { SiteList } from "./Shared/SiteList/SiteList";
 
 export class Contact extends Component {
-    render(){
-        return(
-            <div>
-                <h1>연락처</h1>
-                <p>
-                    연락처들어오는곳
-                </p>
-                <ContactDetails></ContactDetails>
-            </div>
-        );
-    }
+  render() {
+    const title = "연락처.";
+    const description = "ReactMemo 사이트입니다";
+    return (
+      <div>
+        <h2>{title}</h2>
+        <h3>{description}</h3>
+        <ContactDetails></ContactDetails>
+
+        <hr className="m-4"/>
+
+        <div className="alert alert-warning alert-dismissable">
+          <button
+            type="button"
+            className="close"
+            data-dismiss="alert"
+            aria-hidden="true"
+          >
+          </button>
+          이 사이트 제작을 위한 모든 단계는 <strong>데브렉</strong>에서 동영상
+          강좌로 제공됩니다. 관심있으신 분들은{" "}
+          <a href="http://www.devlec.com" className="alert-link">
+            데브렉사이트
+          </a>
+          를 참고하시기바랍니다
+        </div>
+
+        <hr className="m-4"/>
+
+        <div className="row">
+          <div className="col-md-6">
+            <h3>
+              <i className="fa fa-wrench"></i> 현재 사이트에서 사용된 기술리스트
+            </h3>
+            <TechList></TechList>
+          </div>
+          <div className="col-md-6">
+            <h3>
+              <i className="fa fa-sitemap"></i> 현재 사이트와 관련된 추천사이트
+            </h3>
+            <SiteList></SiteList>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
 }
