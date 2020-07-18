7da0bfde3cc8d9831ae48fc8e9d77648199d7f90
hgh <ymca0147@naver.com>
2020-07-18 15:36:12 +0900

    ADD ABOUT PAGE

---
+++ ReactMemo/ClientApp/src/components/About.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+
+export class About extends Component{
+    render(){
+        return (
+            <div>
+                <h1>About ME</h1>
+            </div>
+        );
+    }
+}
